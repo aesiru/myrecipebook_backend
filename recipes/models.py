3839b05cbd28e4d785826c8af6945d1490510181
cy <cyralumacad@gmail.com>
2025-06-05 11:21:53 +0800

    Backend etc

--- recipes/models.py
+++ recipes/models.py
@@ -1,32 +1,32 @@
 from django.db import models
 
 class Ingredient(models.Model):
-    ingredientID = models.CharField(max_length=20, primary_key=True)
-    ingredientName = models.CharField(max_length=50)
+    ingredientID = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
     measure = models.FloatField()
     unit = models.CharField(max_length=10)
 
     def __str__(self):
-        return self.ingredientName
-
+        return self.name
 
 class Recipe(models.Model):
-    recipeID = models.CharField(max_length=20, primary_key=True)
-    cuisineName = models.CharField(max_length=50)
-    categoryName = models.CharField(max_length=20)
-    recipeName = models.CharField(max_length=50)
+    id = models.CharField(primary_key=True, max_length=32)  # Accepts string IDs from frontend
+    recipe_name = models.CharField(max_length=50)
+    cuisine = models.CharField(max_length=50)
+    category = models.CharField(max_length=20)
     duration = models.IntegerField()
     serving = models.IntegerField()
     description = models.TextField()
     procedure = models.TextField()
-    img_url = models.URLField(blank=True, null=True)
+    image = models.URLField(blank=True, null=True)
     favorite = models.BooleanField(default=False)
     date = models.DateField()
     ingredients = models.ManyToManyField(Ingredient, through='Have')
+    steps = models.JSONField(blank=True, default=list)
+    notes = models.TextField(blank=True)
 
     def __str__(self):
-        return self.recipeName
-
+        return self.recipe_name
 
 class Have(models.Model):
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
--- recipes/urls.py
+++ recipes/urls.py
@@ -4,6 +4,6 @@ from .views import RecipeViewSet, IngredientViewSet, HaveViewSet
 router = DefaultRouter()
 router.register(r'recipes', RecipeViewSet)
 router.register(r'ingredients', IngredientViewSet)
-router.register(r'have', HaveViewSet)
+router.register(r'recipe-ingredients', HaveViewSet)  
 
 urlpatterns = router.urls
